1a420647a59c7d5a7c250d2f74b3bb74571c95ed
LMBoyle <boylelukem@gmail.com>
2019-12-29 13:20:31 -0500

    added thumbnails for gallery to speed up site and started on lightbox

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,10 @@ $("span.menu").click(function () {
   $(".top-menu ul").slideToggle("slow", function () {
   });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("loading yall")
+  yall({
+    observeChanges: true
+  });
+});
