b33ad5a417629a1df9ebfb4f801c43b13342cb29
LMBoyle <boylelukem@gmail.com>
2019-12-31 14:32:51 -0500

    function to update modal based on image clicked

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,24 @@
+// Vars ===========================================================================================
+
+let 
+  thisImg,
+  newSrc;
+
+const 
+  modalDiv = $("#lightbox")
+
+// Functions ======================================================================================
+
 //! script-for-menu
-$("span.menu").click(function () {
-  $(".top-menu ul").slideToggle("slow", function () {
+$('span.menu').click(function () {
+  $('.top-menu ul').slideToggle('slow', function () {
   });
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-  console.log("loading yall")
-  yall({
-    observeChanges: true
-  });
-});
+$('#galleryTabContent img').on('click', function() {
+  modalDiv.modal('toggle');
+  thisImg = $(this).attr('src')
+  newSrc = thisImg.split('tn_').join('');
+  modalDiv.find("img").attr('src', newSrc);
+  console.log(modalDiv.find("img"))
+})
