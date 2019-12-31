749026e63f576e403b966db9a2a07b328a62da2f
LMBoyle <boylelukem@gmail.com>
2019-12-31 15:02:50 -0500

    updated the rest of the images and added padding to bottom of gallery

--- scripts/script.js
+++ scripts/script.js
@@ -20,5 +20,4 @@ $('#galleryTabContent img').on('click', function() {
   thisImg = $(this).attr('src')
   newSrc = thisImg.split('tn_').join('');
   modalDiv.find("img").attr('src', newSrc);
-  console.log(modalDiv.find("img"))
 })
